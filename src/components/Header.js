ceb9b7f69d06809e768632a6031fecac917ddf5f
Matteo <matteokocken@gmail.com>
2021-03-25 17:34:05 +0100

    Update Github button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import GitHubButton from 'react-github-btn'
 import '../styles/header.sass';
 
 class Header extends Component {
     render() {
         return (
             <header>
-                <div><button>See on Github</button></div>
+                <div>
+                    <GitHubButton href="https://github.com/matteokocken/SpaceflightApi" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" aria-label="Star matteokocken/SpaceflightApi on GitHub">Voir sur Github</GitHubButton>
+                </div>
                 <h1>Actualités sur les vols spatiaux</h1>
             </header>
         );
